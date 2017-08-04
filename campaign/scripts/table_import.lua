64252d5ecb747ac8f35108130db02faf08f55b88
MikeW <uce_mike@yahoo.com>
2017-08-03 19:38:55 -0500

    table import text
    * import table text completed. From the table records press the "edit"
    pen, then select the "import" option. Paste in text in the text field.
    Every line will be a entry on the table and assigned numbers 1-end of
    list.

--- campaign/scripts/table_import.lua
+++ campaign/scripts/table_import.lua
@@ -6,24 +6,51 @@
 function onInit()
 end
 
--- createTable(1, 1);
-function createTable(nRows, nStep, bSpecial)
-	local node = window.getDatabaseNode().createChild();
+
+function createBlankTable()
+--	local node = window.getDatabaseNode().createChild();
+	local node = getDatabaseNode().createChild();
+--Debug.console("table_import.lua","createTable","node",node);    
+
 	if node then
 		local w = Interface.openWindow("table", node.getNodeName());
-		TableManager.createRows(node, nRows, nStep, bSpecial);
+		--TableManager.createRows(node, nRows, nStep, bSpecial);
 		if w and w.name then
 			w.name.setFocus();
 		end
 	end
+    return node;
 end
 
 function importTextAsTable()
     local sText = tableimporttext.getValue() or "";
 
-Debug.console("table_import.lua","importTextAsTable","sText",sText);    
+--Debug.console("table_import.lua","importTextAsTable","sText",sText);    
 
     if (sText ~= "") then
+        local aTableText = {};
+        for sLine in string.gmatch(sText, '([^\r\n]+)') do
+        table.insert(aTableText, sLine);
+--Debug.console("table_import.lua","importTextAsTable","sLine",sLine);    
+            
+        end
+        
+        local nodeTable = createBlankTable();
+        local nNewRow = 0;
+        if (nodeTable) then
+--Debug.console("table_import.lua","importTextAsTable","nodeTable",nodeTable);    
+            local nodeTableRows = nodeTable.createChild("tablerows");
+--Debug.console("table_import.lua","importTextAsTable","nodeTableRows",nodeTableRows);    
+            for _,sTableLines in ipairs(aTableText) do
+                nNewRow = nNewRow + 1;
+--Debug.console("table_import.lua","importTextAsTable","sTableLines",sTableLines);                    
+                local nodeRow = nodeTableRows.createChild();
+                DB.setValue(nodeRow, "fromrange", "number", nNewRow);
+                DB.setValue(nodeRow, "torange", "number", nNewRow);
+                local nodeResults = nodeRow.createChild("results");
+                local nodeResult = nodeResults.createChild();
+                DB.setValue(nodeResult, "result", "string", sTableLines);
+            end
+        end
     end
-    
 end
