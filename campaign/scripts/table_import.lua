1f69301ab8c4f624c5efee6e3ff8cc6efa6e965f
MikeW <uce_mike@yahoo.com>
2017-08-03 21:39:35 -0500

    table import
    * bugfix on table text import, related to creating extra "new" node.

--- campaign/scripts/table_import.lua
+++ campaign/scripts/table_import.lua
@@ -4,12 +4,15 @@
 --
 
 function onInit()
+--Debug.console("table_import.lua","onInit","window.getDatabaseNode",window.getDatabaseNode());    
+--Debug.console("table_import.lua","onInit","getDatabaseNode",getDatabaseNode());    
 end
 
 
 function createBlankTable()
 --	local node = window.getDatabaseNode().createChild();
-	local node = getDatabaseNode().createChild();
+--	local node = getDatabaseNode().createChild();
+    local node = DB.createChild("tables"); 
 --Debug.console("table_import.lua","createTable","node",node);    
 
 	if node then
@@ -30,20 +33,15 @@ function importTextAsTable()
     if (sText ~= "") then
         local aTableText = {};
         for sLine in string.gmatch(sText, '([^\r\n]+)') do
-        table.insert(aTableText, sLine);
---Debug.console("table_import.lua","importTextAsTable","sLine",sLine);    
-            
+            table.insert(aTableText, sLine);
         end
         
         local nodeTable = createBlankTable();
         local nNewRow = 0;
         if (nodeTable) then
---Debug.console("table_import.lua","importTextAsTable","nodeTable",nodeTable);    
             local nodeTableRows = nodeTable.createChild("tablerows");
---Debug.console("table_import.lua","importTextAsTable","nodeTableRows",nodeTableRows);    
             for _,sTableLines in ipairs(aTableText) do
                 nNewRow = nNewRow + 1;
---Debug.console("table_import.lua","importTextAsTable","sTableLines",sTableLines);                    
                 local nodeRow = nodeTableRows.createChild();
                 DB.setValue(nodeRow, "fromrange", "number", nNewRow);
                 DB.setValue(nodeRow, "torange", "number", nNewRow);
