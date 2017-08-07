832805c94c3979a5d3626fdba559daa4f2d978da
MikeW <uce_mike@yahoo.com>
2017-08-06 20:31:46 -0500

    npc
    * trimmed out some 5e fields in npc records since actions tab
    (power/weapons) replaced it.

--- campaign/scripts/npc_main.lua
+++ campaign/scripts/npc_main.lua
@@ -105,152 +105,152 @@ function update()
 	cr.setReadOnly(bReadOnly);
 	xp.setReadOnly(bReadOnly);
 	
-	if bReadOnly then
-		if traits_iedit then
-			traits_iedit.setValue(0);
-			traits_iedit.setVisible(false);
-		end
+	-- if bReadOnly then
+		-- if traits_iedit then
+			-- traits_iedit.setValue(0);
+			-- traits_iedit.setVisible(false);
+		-- end
 		
-		local bShow = (traits.getWindowCount() ~= 0);
-		header_traits.setVisible(bShow);
-		traits.setVisible(bShow);
-	else
-		if traits_iedit then
-			traits_iedit.setVisible(true);
-		end
-		header_traits.setVisible(true);
-		traits.setVisible(true);
-	end
-	for _,w in ipairs(traits.getWindows()) do
-		w.name.setReadOnly(bReadOnly);
-		w.desc.setReadOnly(bReadOnly);
-	end
+		-- local bShow = (traits.getWindowCount() ~= 0);
+		-- header_traits.setVisible(bShow);
+		-- traits.setVisible(bShow);
+	-- else
+		-- if traits_iedit then
+			-- traits_iedit.setVisible(true);
+		-- end
+		-- header_traits.setVisible(true);
+		-- traits.setVisible(true);
+	-- end
+	-- for _,w in ipairs(traits.getWindows()) do
+		-- w.name.setReadOnly(bReadOnly);
+		-- w.desc.setReadOnly(bReadOnly);
+	-- end
 
-	if bReadOnly then
-		if actions_iedit then
-			actions_iedit.setValue(0);
-			actions_iedit.setVisible(false);
-		end
+	-- if bReadOnly then
+		-- if actions_iedit then
+			-- actions_iedit.setValue(0);
+			-- actions_iedit.setVisible(false);
+		-- end
 		
-		local bShow = (actions.getWindowCount() ~= 0);
-		header_actions.setVisible(bShow);
-		actions.setVisible(bShow);
-	else
-		if actions_iedit then
-			actions_iedit.setVisible(true);
-		end
-		header_actions.setVisible(true);
-		actions.setVisible(true);
-	end
-	for _,w in ipairs(actions.getWindows()) do
-		w.name.setReadOnly(bReadOnly);
-		w.desc.setReadOnly(bReadOnly);
-	end
+		-- local bShow = (actions.getWindowCount() ~= 0);
+		-- header_actions.setVisible(bShow);
+		-- actions.setVisible(bShow);
+	-- else
+		-- if actions_iedit then
+			-- actions_iedit.setVisible(true);
+		-- end
+		-- header_actions.setVisible(true);
+		-- actions.setVisible(true);
+	-- end
+	-- for _,w in ipairs(actions.getWindows()) do
+		-- w.name.setReadOnly(bReadOnly);
+		-- w.desc.setReadOnly(bReadOnly);
+	-- end
 	
-	if bReadOnly then
-		if reactions_iedit then
-			reactions_iedit.setValue(0);
-			reactions_iedit.setVisible(false);
-		end
+	-- if bReadOnly then
+		-- if reactions_iedit then
+			-- reactions_iedit.setValue(0);
+			-- reactions_iedit.setVisible(false);
+		-- end
 		
-		local bShow = (reactions.getWindowCount() ~= 0);
-		header_reactions.setVisible(bShow);
-		reactions.setVisible(bShow);
-	else
-		if reactions_iedit then
-			reactions_iedit.setVisible(true);
-		end
-		header_reactions.setVisible(true);
-		reactions.setVisible(true);
-	end
-	for _,w in ipairs(reactions.getWindows()) do
-		w.name.setReadOnly(bReadOnly);
-		w.desc.setReadOnly(bReadOnly);
-	end
+		-- local bShow = (reactions.getWindowCount() ~= 0);
+		-- header_reactions.setVisible(bShow);
+		-- reactions.setVisible(bShow);
+	-- else
+		-- if reactions_iedit then
+			-- reactions_iedit.setVisible(true);
+		-- end
+		-- header_reactions.setVisible(true);
+		-- reactions.setVisible(true);
+	-- end
+	-- for _,w in ipairs(reactions.getWindows()) do
+		-- w.name.setReadOnly(bReadOnly);
+		-- w.desc.setReadOnly(bReadOnly);
+	-- end
 	
-	if bReadOnly then
-		if legendaryactions_iedit then
-			legendaryactions_iedit.setValue(0);
-			legendaryactions_iedit.setVisible(false);
-		end
+	-- if bReadOnly then
+		-- if legendaryactions_iedit then
+			-- legendaryactions_iedit.setValue(0);
+			-- legendaryactions_iedit.setVisible(false);
+		-- end
 		
-		local bShow = (legendaryactions.getWindowCount() ~= 0);
-		header_legendaryactions.setVisible(bShow);
-		legendaryactions.setVisible(bShow);
-	else
-		if legendaryactions_iedit then
-			legendaryactions_iedit.setVisible(true);
-		end
-		header_legendaryactions.setVisible(true);
-		legendaryactions.setVisible(true);
-	end
-	for _,w in ipairs(legendaryactions.getWindows()) do
-		w.name.setReadOnly(bReadOnly);
-		w.desc.setReadOnly(bReadOnly);
-	end
+		-- local bShow = (legendaryactions.getWindowCount() ~= 0);
+		-- header_legendaryactions.setVisible(bShow);
+		-- legendaryactions.setVisible(bShow);
+	-- else
+		-- if legendaryactions_iedit then
+			-- legendaryactions_iedit.setVisible(true);
+		-- end
+		-- header_legendaryactions.setVisible(true);
+		-- legendaryactions.setVisible(true);
+	-- end
+	-- for _,w in ipairs(legendaryactions.getWindows()) do
+		-- w.name.setReadOnly(bReadOnly);
+		-- w.desc.setReadOnly(bReadOnly);
+	-- end
 	
-	if bReadOnly then
-		if lairactions_iedit then
-			lairactions_iedit.setValue(0);
-			lairactions_iedit.setVisible(false);
-		end
+	-- if bReadOnly then
+		-- if lairactions_iedit then
+			-- lairactions_iedit.setValue(0);
+			-- lairactions_iedit.setVisible(false);
+		-- end
 		
-		local bShow = (lairactions.getWindowCount() ~= 0);
-		header_lairactions.setVisible(bShow);
-		lairactions.setVisible(bShow);
-	else
-		if lairactions_iedit then
-			lairactions_iedit.setVisible(true);
-		end
-		header_lairactions.setVisible(true);
-		lairactions.setVisible(true);
-	end
-	for _,w in ipairs(lairactions.getWindows()) do
-		w.name.setReadOnly(bReadOnly);
-		w.desc.setReadOnly(bReadOnly);
-	end
+		-- local bShow = (lairactions.getWindowCount() ~= 0);
+		-- header_lairactions.setVisible(bShow);
+		-- lairactions.setVisible(bShow);
+	-- else
+		-- if lairactions_iedit then
+			-- lairactions_iedit.setVisible(true);
+		-- end
+		-- header_lairactions.setVisible(true);
+		-- lairactions.setVisible(true);
+	-- end
+	-- for _,w in ipairs(lairactions.getWindows()) do
+		-- w.name.setReadOnly(bReadOnly);
+		-- w.desc.setReadOnly(bReadOnly);
+	-- end
 	
-	if bReadOnly then
-		if innatespells_iedit then
-			innatespells_iedit.setValue(0);
-			innatespells_iedit.setVisible(false);
-		end
+	-- if bReadOnly then
+		-- if innatespells_iedit then
+			-- innatespells_iedit.setValue(0);
+			-- innatespells_iedit.setVisible(false);
+		-- end
 		
-		local bShow = (innatespells.getWindowCount() ~= 0);
-		header_innatespells.setVisible(bShow);
-		innatespells.setVisible(bShow);
-	else
-		if innatespells_iedit then
-			innatespells_iedit.setVisible(true);
-		end
-		header_innatespells.setVisible(true);
-		innatespells.setVisible(true);
-	end
-	for _,w in ipairs(innatespells.getWindows()) do
-		w.name.setReadOnly(bReadOnly);
-		w.desc.setReadOnly(bReadOnly);
-	end
+		-- local bShow = (innatespells.getWindowCount() ~= 0);
+		-- header_innatespells.setVisible(bShow);
+		-- innatespells.setVisible(bShow);
+	-- else
+		-- if innatespells_iedit then
+			-- innatespells_iedit.setVisible(true);
+		-- end
+		-- header_innatespells.setVisible(true);
+		-- innatespells.setVisible(true);
+	-- end
+	-- for _,w in ipairs(innatespells.getWindows()) do
+		-- w.name.setReadOnly(bReadOnly);
+		-- w.desc.setReadOnly(bReadOnly);
+	-- end
 	
-	if bReadOnly then
-		if spells_iedit then
-			spells_iedit.setValue(0);
-			spells_iedit.setVisible(false);
-		end
+	-- if bReadOnly then
+		-- if spells_iedit then
+			-- spells_iedit.setValue(0);
+			-- spells_iedit.setVisible(false);
+		-- end
 		
-		local bShow = (spells.getWindowCount() ~= 0);
-		header_spells.setVisible(bShow);
-		spells.setVisible(bShow);
-	else
-		if spells_iedit then
-			spells_iedit.setVisible(true);
-		end
-		header_spells.setVisible(true);
-		spells.setVisible(true);
-	end
-	for _,w in ipairs(spells.getWindows()) do
-		w.name.setReadOnly(bReadOnly);
-		w.desc.setReadOnly(bReadOnly);
-	end
+		-- local bShow = (spells.getWindowCount() ~= 0);
+		-- header_spells.setVisible(bShow);
+		-- spells.setVisible(bShow);
+	-- else
+		-- if spells_iedit then
+			-- spells_iedit.setVisible(true);
+		-- end
+		-- header_spells.setVisible(true);
+		-- spells.setVisible(true);
+	-- end
+	-- for _,w in ipairs(spells.getWindows()) do
+		-- w.name.setReadOnly(bReadOnly);
+		-- w.desc.setReadOnly(bReadOnly);
+	-- end
 end
 
 function addTrait(sName, sDesc)
@@ -329,11 +329,11 @@ function onDrop(x, y, draginfo)
 		elseif sClass == "reference_backgroundfeature" then
 			addAction(DB.getValue(nodeSource, "name", ""), DB.getText(nodeSource, "text", ""));
 		elseif sClass == "reference_classfeature" then
-			addAction(DB.getValue(nodeSource, "name", ""), DB.getText(nodeSource, "text", ""));
+			--addAction(DB.getValue(nodeSource, "name", ""), DB.getText(nodeSource, "text", ""));
 		elseif sClass == "reference_feat" then
-			addAction(DB.getValue(nodeSource, "name", ""), DB.getText(nodeSource, "text", ""));
+			--addAction(DB.getValue(nodeSource, "name", ""), DB.getText(nodeSource, "text", ""));
 		elseif sClass == "reference_racialtrait" or sClass == "reference_subracialtrait" then
-			addTrait(DB.getValue(nodeSource, "name", ""), DB.getText(nodeSource, "text", ""));
+			--addTrait(DB.getValue(nodeSource, "name", ""), DB.getText(nodeSource, "text", ""));
 		end
 		return true;
 	end
