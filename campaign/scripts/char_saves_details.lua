be2e29594b8dd8ccbbfb2286a7517244dcc5ffd1
MikeW <uce_mike@yahoo.com>
2017-08-17 18:53:45 -0500

    details
    * window details for abilities and saves, compacted them now that there
    isn't a large number of modifiers.

--- campaign/scripts/char_saves_details.lua
+++ campaign/scripts/char_saves_details.lua
@@ -1,5 +1,5 @@
 --
--- for AD&D Core, abilities menu
+-- for AD&D Core, saves
 --
 --
 
@@ -11,10 +11,7 @@ function onInit()
 
     local sTarget = string.lower(self.target[1]);
     DB.addHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".base"),       "onUpdate", detailsUpdate);
-    DB.addHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".baseitem"),    "onUpdate", detailsUpdate);
     DB.addHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".basemod"),    "onUpdate", detailsUpdate);
-    DB.addHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".itemmod"),    "onUpdate", detailsUpdate);
-    DB.addHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".effectmod"),  "onUpdate", detailsUpdate);
     DB.addHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".adjustment"), "onUpdate", detailsUpdate);
     DB.addHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".tempmod"),    "onUpdate", detailsUpdate);
     detailsUpdate();
@@ -22,14 +19,9 @@ end
 function onClose()
     local nodeChar = window.getDatabaseNode();
 
-    --Debug.console("char_abilities_details.lua","onClose","nodeChar",nodeChar);
-
     local sTarget = string.lower(self.target[1]);
     DB.removeHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".base"),       "onUpdate", detailsUpdate);
-    DB.removeHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".baseitem"),    "onUpdate", detailsUpdate);
     DB.removeHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".basemod"),    "onUpdate", detailsUpdate);
-    DB.removeHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".itemmod"),    "onUpdate", detailsUpdate);
-    DB.removeHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".effectmod"),  "onUpdate", detailsUpdate);
     DB.removeHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".adjustment"), "onUpdate", detailsUpdate);
     DB.removeHandler(DB.getPath(nodeChar, "saves." .. sTarget .. ".tempmod"),    "onUpdate", detailsUpdate);
 end
@@ -54,7 +46,7 @@ end
 
 
 function detailsUpdate()
-    local node = getDatabaseNode();
+    --local node = getDatabaseNode();
     local nodeChar = window.getDatabaseNode();
     local sTarget = string.lower(self.target[1]);
     
@@ -62,10 +54,7 @@ function detailsUpdate()
     --Debug.console("char_abilities_details.lua","detailsUpdate","sTarget",sTarget);
     
     local nBase =       DB.getValue(nodeChar, "saves." .. sTarget .. ".base",20);
-    local nBaseItem =    DB.getValue(nodeChar, "saves." .. sTarget .. ".baseitem",0);
     local nBaseMod =    DB.getValue(nodeChar, "saves." .. sTarget .. ".basemod",0);
-    local nItemMod =    DB.getValue(nodeChar, "saves." .. sTarget .. ".itemmod",0);
-    local nEffectMod =  DB.getValue(nodeChar, "saves." .. sTarget .. ".effectmod",0);
     local nAdjustment = DB.getValue(nodeChar, "saves." .. sTarget .. ".adjustment",0);
     local nTempMod =    DB.getValue(nodeChar, "saves." .. sTarget .. ".tempmod",0);
     local nFinalBase = nBase;
@@ -73,13 +62,9 @@ function detailsUpdate()
     if (nBaseMod ~= 0) then
         nFinalBase = nBaseMod;
     end
-    if (nBaseItem > nBaseMod) then
-        nFinalBase = nBaseItem;
-    end
-    
     -- flip negative to positive, since we expect +2 to be better and -2 to be worse
     -- here.
-    local nFlipMods = ( (nItemMod + nEffectMod + nAdjustment + nTempMod) * -1);
+    local nFlipMods = ( (nAdjustment + nTempMod) * -1);
     local nTotal = (nFinalBase + nFlipMods);
     if (nTotal < 1) then
         nTotal = 1;
