f1718db1c499b72e385b8a1c8628da4a3ebf24d4
MikeW <uce_mike@yahoo.com>
2017-09-21 18:25:00 -0500

    CT
    * effects, client side fixes related to effect removal features new to
    CoreRPG.

--- ct/scripts/clientct_entry.lua
+++ ct/scripts/clientct_entry.lua
@@ -8,48 +8,6 @@ function onInit()
 	onHealthChanged();
 end
 
-function updateDisplay()
-	local sFaction = friendfoe.getStringValue();
-
-	local sOptCTSI = OptionsManager.getOption("CTSI");
-	local bShowInit = ((sOptCTSI == "friend") and (sFaction == "friend")) or (sOptCTSI == "on");
-	initresult.setVisible(bShowInit);
-	
-	if active.getValue() == 1 then
-		name.setFont("sheetlabel");
-
-		active_spacer_top.setVisible(true);
-		active_spacer_bottom.setVisible(true);
-		
-		if sFaction == "friend" then
-			setFrame("ctentrybox_friend_active");
-		elseif sFaction == "neutral" then
-			setFrame("ctentrybox_neutral_active");
-		elseif sFaction == "foe" then
-			setFrame("ctentrybox_foe_active");
-		else
-			setFrame("ctentrybox_active");
-		end
-
-		windowlist.scrollToWindow(self);
-	else
-		name.setFont("sheettext");
-
-		active_spacer_top.setVisible(false);
-		active_spacer_bottom.setVisible(false);
-		
-		if sFaction == "friend" then
-			setFrame("ctentrybox_friend");
-		elseif sFaction == "neutral" then
-			setFrame("ctentrybox_neutral");
-		elseif sFaction == "foe" then
-			setFrame("ctentrybox_foe");
-		else
-			setFrame("ctentrybox");
-		end
-	end
-end
-
 function onActiveChanged()
 	updateDisplay();
 end
@@ -98,4 +56,3 @@ function updateHealthDisplay()
 		status.setVisible(false);
 	end
 end
-
