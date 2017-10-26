effedf7e0d17dc752b6649ffb6d7501143525347
MikeW <uce_mike@yahoo.com>
2017-10-26 14:06:56 -0500

    table_import
    * first line of import text is the name of the table, rest are table
    entries.

--- campaign/scripts/table_import.lua
+++ campaign/scripts/table_import.lua
@@ -27,27 +27,27 @@ end
 
 function importTextAsTable()
     local sText = tableimporttext.getValue() or "";
-
 --Debug.console("table_import.lua","importTextAsTable","sText",sText);    
-
     if (sText ~= "") then
         local aTableText = {};
         for sLine in string.gmatch(sText, '([^\r\n]+)') do
             table.insert(aTableText, sLine);
         end
-        
         local nodeTable = createBlankTable();
+        DB.setValue(nodeTable,"name","string",aTableText[1]);
         local nNewRow = 0;
         if (nodeTable) then
             local nodeTableRows = nodeTable.createChild("tablerows");
             for _,sTableLines in ipairs(aTableText) do
                 nNewRow = nNewRow + 1;
-                local nodeRow = nodeTableRows.createChild();
-                DB.setValue(nodeRow, "fromrange", "number", nNewRow);
-                DB.setValue(nodeRow, "torange", "number", nNewRow);
-                local nodeResults = nodeRow.createChild("results");
-                local nodeResult = nodeResults.createChild();
-                DB.setValue(nodeResult, "result", "string", sTableLines);
+                if (nNewRow ~= 1) then -- use line 1 as title/name of table
+                    local nodeRow = nodeTableRows.createChild();
+                    DB.setValue(nodeRow, "fromrange", "number", nNewRow-1);
+                    DB.setValue(nodeRow, "torange", "number", nNewRow-1);
+                    local nodeResults = nodeRow.createChild("results");
+                    local nodeResult = nodeResults.createChild();
+                    DB.setValue(nodeResult, "result", "string", sTableLines);
+                end
             end
         end
     end
