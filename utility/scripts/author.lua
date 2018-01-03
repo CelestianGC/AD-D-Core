ef94a2b0e870cbb1b4ecfb7ea3d362bad6916cd4
MikeW <uce_mike@yahoo.com>
2018-01-03 01:27:57 -0600

    /author, fixed sorting when writing to ref-manual
    /author, fixed sorting when writing to ref-manual

--- utility/scripts/author.lua
+++ utility/scripts/author.lua
@@ -1,10 +1,20 @@
 --
---
+-- Export Story text as ref-manual with records
 --
 --
 --
 
+-- pass list of nodes with a "name" record and sort by name
+function sortByName(nodes)
+        local aSorted = {};
+        for _,node in pairs(nodes) do
+            table.insert(aSorted, node);
+        end        
+        table.sort(aSorted, function (a, b) return DB.getValue(a,"name","") < DB.getValue(b,"name","") end);
+        return aSorted;
+end
 
+-- perform the export to Ref-Manual fields.
 function performExport()
     aProperties = {};
 	aProperties.name = name.getValue();
@@ -21,12 +31,10 @@ function performExport()
     
     local dStoryRaw = DB.getChildren("encounter");
 Debug.console("author.lua","performExport","------>dStoryRaw",dStoryRaw);
-    --local dCategoriesRaw = DB.getChildCategories(dStoryRaw,true);
---Debug.console("author.lua","performExport","------>dCategoriesRaw",dCategoriesRaw);
     local dRoot = DB.createChild("_authorRefmanual_tmp");
     local dStories = DB.createChild(dRoot,"stories");
     local dStoryCategories = DB.createChild(dStories,"category");
-    
+
     for _,node in pairs(dStoryRaw) do
         local sCategory = UtilityManager.getNodeCategory(node);
         local dCategory = DB.getChild(dStoryCategories,sCategory);
@@ -36,10 +44,9 @@ Debug.console("author.lua","performExport","------>dStoryRaw",dStoryRaw);
         end
         local nodeEntry = dCategory.createChild();
         DB.copyNode(node,nodeEntry);
-Debug.console("author.lua","performExport","sCategory",sCategory);
-Debug.console("author.lua","performExport","dCategory",dCategory);
-Debug.console("author.lua","performExport","nodeEntry",nodeEntry);
-Debug.console("author.lua","performExport","node",node);
+-- Debug.console("author.lua","performExport","sCategory",sCategory);
+-- Debug.console("author.lua","performExport","dCategory",dCategory);
+-- Debug.console("author.lua","performExport","nodeEntry",nodeEntry);
     end
 
     -- create root "author" node to drop entries
@@ -51,7 +58,6 @@ Debug.console("author.lua","performExport","node",node);
     DB.setValue(nodeLibrary,"categoryname","string",aProperties.category);
     DB.setValue(nodeLibrary,"name","string","Name-Ref-Manual-PlaceHolder-Text");
     local nodeLibraryEntries =  DB.createChild(nodeLibrary,"entries");
-    --local nodeLibraryEntry =  DB.createChild(nodeLibraryEntries,"ref_00001");
     local nodeLibraryEntry =  DB.createChild(nodeLibraryEntries);
     DB.setValue(nodeLibraryEntry,"name","string",aProperties.name);
     local sLinkClass = "reference_manual";
@@ -79,8 +85,9 @@ Debug.console("author.lua","performExport","node",node);
                     DB.setValue(nodeLibraryAdditional,"librarylink","windowreference",sClass,sRecord);
 					for _,nodeChild in pairs(nodeSource.getChildren()) do
                         if nodeChild.getType() == "node" then
-                            local nodeAuthor = dAuthorRecord.createChild();
-                            DB.copyNode(nodeChild,nodeAuthor);
+                            -- keep same path to records so links work in stories/pages
+                            local sNodePath = dAuthorNode.getPath() .. "." .. nodeChild.getPath();
+                            DB.copyNode(nodeChild,sNodePath);
                         end
                     end
                 end
@@ -92,31 +99,31 @@ Debug.console("author.lua","performExport","node",node);
     local dReference = DB.createChild(dAuthorNode,"reference");
     local nodeRefIndex = DB.createChild(dReference,"refmanualindex");
     local nodeChapters = DB.createChild(nodeRefIndex,"chapters");
-    --local nodeChapter = DB.createChild(nodeChapters,"chapter");
     local nodeChapter = DB.createChild(nodeChapters);
     DB.setValue(nodeChapter,"name","string",aProperties.name);
     local nodeSubChapters = DB.createChild(nodeChapter,"subchapters");
     -- flip through all categories, create sub per category and and entries within category
     for _,nodeCategory in pairs(dStoryCategories.getChildren()) do
         -- create subchapter node and set name
-        --local nodeSubChapter = DB.createChild(nodeSubChapters,"sub_chapter");
         local nodeSubChapter = DB.createChild(nodeSubChapters);
         DB.setValue(nodeSubChapter,"name","string",DB.getValue(nodeCategory,"name","EMPTY-CATEGORY-NAME"));
-        for _,nodeStory in pairs(nodeCategory.getChildren()) do
+        for _,nodeStory in pairs(sortByName(nodeCategory.getChildren())) do
             -- create refpages node and current node to work on and set name/links
             local dRefPages = DB.createChild(nodeSubChapter,"refpages");
-            --local nodeRefPage = DB.createChild(dRefPages,"refpage");
-            local nodeRefPage = DB.createChild(dRefPages);
-            DB.setValue(nodeRefPage,"name","string",DB.getValue(nodeStory,"name","EMPTY-STORY-NAME"));
-            DB.setValue(nodeRefPage,"keywords","string",DB.getValue(nodeStory,"name","EMPTY-STORY-NAME"));
-            local sLinkClass = "reference_manualtextwide";
-            local sLinkRecord = "..";
-            DB.setValue(nodeRefPage,"listlink","windowreference",sLinkClass,sLinkRecord);
-            -- create block node and set text from story
-            local dBlocks = DB.createChild(nodeRefPage,"blocks");
-            --local nodeBlock = DB.createChild(dBlocks,"block");
-            local nodeBlock = DB.createChild(dBlocks);
-            DB.setValue(nodeBlock,"text","formattedtext",DB.getValue(nodeStory,"text","EMPTY-STORY-TEXT"));
+            local sNodeName = DB.getValue(nodeStory,"name","");
+            if (sNodeName ~= "") then
+                local nodeRefPage = DB.createChild(dRefPages);
+                DB.setValue(nodeRefPage,"name","string",sNodeName);
+                DB.setValue(nodeRefPage,"keywords","string",sNodeName);
+                local sLinkClass = "reference_manualtextwide";
+                local sLinkRecord = "..";
+                DB.setValue(nodeRefPage,"listlink","windowreference",sLinkClass,sLinkRecord);
+                -- create block node and set text from story
+                local dBlocks = DB.createChild(nodeRefPage,"blocks");
+                --local nodeBlock = DB.createChild(dBlocks,"block");
+                local nodeBlock = DB.createChild(dBlocks);
+                DB.setValue(nodeBlock,"text","formattedtext",DB.getValue(nodeStory,"text","EMPTY-STORY-TEXT"));
+            end
         end
     end
 
