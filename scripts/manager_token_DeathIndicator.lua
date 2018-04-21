41bda65507ee50bbc3bf35192d15d8adf3fa8238
MikeW <uce_mike@yahoo.com>
2018-04-21 14:01:52 -0500

    bugfix: RIP tokens on loading FG didn't show if tokens already there
    bugfix: RIP tokens on loading FG didn't show if tokens already there

--- scripts/manager_token_DeathIndicator.lua
+++ scripts/manager_token_DeathIndicator.lua
@@ -5,43 +5,60 @@ function onInit()
 			{ labels = "option_val_on", values = "on", baselabel = "option_val_off", baseval = "off", default = "off" });
   
   CombatManager.addCombatantFieldChangeHandler("wounds", "onUpdate", updateHealth);
-	DB.addHandler("options.COMBAT_SHOW_RIP", "onUpdate", TokenManager.onOptionChanged);
-	DB.addHandler("options.COMBAT_SHOW_RIP_DM", "onUpdate", TokenManager.onOptionChanged);  
+  CombatManager.addCombatantFieldChangeHandler("tokenrefid", "onUpdate", updateHealth);
+	--DB.addHandler("options.COMBAT_SHOW_RIP", "onUpdate", TokenManager.onOptionChanged);
+	--DB.addHandler("options.COMBAT_SHOW_RIP_DM", "onUpdate", TokenManager.onOptionChanged);  
+  
+  -- for when options are toggled in settings.
+	DB.addHandler("options.COMBAT_SHOW_RIP", "onUpdate", updateCTEntries);
+	DB.addHandler("options.COMBAT_SHOW_RIP_DM", "onUpdate", updateCTEntries);  
+  -- for when FG starts.
+  updateCTEntries();
+end
+
+function updateCTEntries()
+	for _,node in pairs(CombatManager.getCombatantNodes()) do
+		updateHealth(node.getChild("wounds"));
+	end
 end
 
 -- various updates thanks to suggestions from Andraax
 function updateHealth(nodeField)
 	local nodeCT = nodeField.getParent();
 	local tokenCT = CombatManager.getTokenFromCT(nodeCT);
-	-- Percent Damage, Status String, Wound Color
-	local pDmg, pStatus, sColor = TokenManager2.getHealthInfo(nodeCT);
-	-- show rip on tokens
-	local bOptionShowRIP = OptionsManager.isOption("COMBAT_SHOW_RIP", "on");
-	local bOptionShowRIP_DM = OptionsManager.isOption("COMBAT_SHOW_RIP_DM", "on");
-	-- new stuff, adds indicator for "DEAD" on the token. -celestian
-	local widgetDeathIndicator = tokenCT.findWidget("deathindicator");
-	local nWidth, nHeight = tokenCT.getSize();
-	local sName = DB.getValue(nodeCT,"name","Unknown");
-	local sDeathTokenName = "token_dead";
-  -- sDeathTokenName = sDeathTokenName .. tostring(math.random(5)); -- creates token_dead0,token_dead1,token_dead2,token_dead3,token_dead4,token_dead5 string
-  -- figure out if this is a pc token
-  local rActor = ActorManager.getActorFromCT(nodeCT);
-  local sActorType, nodeActor = ActorManager.getTypeAndNode(rActor);    
-  if sActorType == "pc" then
-    sDeathTokenName = "token_dead_pc";
+  if (tokenCT) then
+    -- Percent Damage, Status String, Wound Color
+    local pDmg, pStatus, sColor = TokenManager2.getHealthInfo(nodeCT);
+    
+    -- new stuff, adds indicator for "DEAD" on the token. -celestian
+    local sDeathTokenName = "token_dead";
+    -- sDeathTokenName = sDeathTokenName .. tostring(math.random(5)); -- creates token_dead0,token_dead1,token_dead2,token_dead3,token_dead4,token_dead5 string
+    -- figure out if this is a pc token
+    local rActor = ActorManager.getActorFromCT(nodeCT);
+    local sActorType, nodeActor = ActorManager.getTypeAndNode(rActor);    
+    if sActorType == "pc" then
+      sDeathTokenName = "token_dead_pc";
+    end
+    
+    local widgetDeathIndicator = tokenCT.findWidget("deathindicator");
+    if not widgetDeathIndicator then
+      local nWidth, nHeight = tokenCT.getSize();
+      local sName = DB.getValue(nodeCT,"name","Unknown");
+      widgetDeathIndicator = tokenCT.addBitmapWidget(sDeathTokenName);
+      widgetDeathIndicator.setBitmap(sDeathTokenName);
+      widgetDeathIndicator.setName("deathindicator");
+      widgetDeathIndicator.setTooltipText(sName .. " has fallen, as if dead.");
+      widgetDeathIndicator.setSize(nWidth-20, nHeight-20);
+    end
+    
+    -- show rip on tokens
+    local bOptionShowRIP = OptionsManager.isOption("COMBAT_SHOW_RIP", "on");
+    local bOptionShowRIP_DM = OptionsManager.isOption("COMBAT_SHOW_RIP_DM", "on");
+    -- display if health 0 or lower and option on
+    local bPlayDead = ((pDmg >= 1) and (bOptionShowRIP));
+    if User.isHost() then
+      bPlayDead = ((pDmg >= 1) and (bOptionShowRIP_DM));
+    end
+    widgetDeathIndicator.setVisible(bPlayDead);
   end
-	-- display if health 0 or lower and option on
-	local bPlayDead = ((pDmg >= 1) and (bOptionShowRIP));
-	if User.isHost() then
-		bPlayDead = ((pDmg >= 1) and (bOptionShowRIP_DM));
-	end
-  
-	if not widgetDeathIndicator then
-		widgetDeathIndicator = tokenCT.addBitmapWidget(sDeathTokenName);
-		widgetDeathIndicator.setBitmap(sDeathTokenName);
-		widgetDeathIndicator.setName("deathindicator");
-		widgetDeathIndicator.setTooltipText(sName .. " has fallen, as if dead.");
-		widgetDeathIndicator.setSize(nWidth-20, nHeight-20);
-	end
-	widgetDeathIndicator.setVisible(bPlayDead);
 end
